2dbcc92e8c1f3934e173d5843df2217fe9db5af7
Alexander Danilychev Jr <danilychev@berkeley.edu>
2016-03-07 21:06:39 -0800

    possibly fixed it (using values_list and assuming pk is there)

--- django_pandas/io.py
+++ django_pandas/io.py
@@ -104,23 +104,51 @@ def read_frame(qs, fieldnames=(), index_col=None, coerce_float=False,
 
     if is_values_queryset(qs):
         recs = list(qs)
+        printFlush("Not efficiently iterating bc is values queryset")
+        recs2 = list(qs)
     else:
         recs = list(iterateEfficiently(qs, fieldnames))
         #print "Efficient? " + str(list(recs))
-        #recs = list(qs.values_list(*fieldnames))
+        #recs2 = list(qs.values_list(*fieldnames))
         #print recs
 
     df = pd.DataFrame.from_records(recs, columns=fieldnames,
                                    coerce_float=coerce_float)
+    #df2 = pd.DataFrame.from_records(recs2, columns=fieldnames,
+                                   coerce_float=coerce_float)
+    #printFlush(df.to_string())
+    #printFlush(df2.to_string())
     if verbose:
         update_with_verbose(df, fieldnames, fields)
 
     if index_col is not None:
         df.set_index(index_col, inplace=True)
-
     return df
 
 def iterateEfficiently(qs, fieldnames, chunksize=1000, reverse=False):
+    ordering =""
+    printFlush(fieldnames)
+    printFlush(type(fieldnames))
+    fieldnames = ['pk'] + list(fieldnames)
+    qs = qs.order_by(ordering + 'pk')
+    last_pk = None
+    new_items = True
+    while new_items:
+        new_items = False
+        chunk = qs
+        if last_pk is not None:
+            func = 'lt' if reverse else 'gt'
+            chunk = chunk.filter(**{'pk__' + func: last_pk})
+        chunk = chunk[:chunksize]
+        row = None
+        printFlush("iterateEfficiently!!!!")
+        for row in chunk.values_list(*fieldnames):
+            yield row
+        if row is not None:
+            last_pk = row[0]
+            new_items = True
+
+def iterateEfficientlyNoFields(qs, chunksize=1000, reverse=False):
     ordering =""
     qs = qs.order_by(ordering + 'pk')
     last_pk = None
@@ -130,11 +158,11 @@ def iterateEfficiently(qs, fieldnames, chunksize=1000, reverse=False):
         chunk = qs
         if last_pk is not None:
             func = 'lt' if reverse else 'gt'
-            chunk = chunk.filter(**{'pk__' + func: last_pk})#.values(*fieldnames)
+            chunk = chunk.filter(**{'pk__' + func: last_pk})
         chunk = chunk[:chunksize]
         row = None
         printFlush("iterateEfficiently!!!!")
-        for row in chunk.values_list(*fieldnames):
+        for row in chunk:
             yield row
         if row is not None:
             last_pk = row.pk
